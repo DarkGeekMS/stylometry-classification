8eeea1471ad630d75adc66a0d379b56829de43ee
DarkGeekMS <mohamedshawky911@gmail.com>
2020-12-22 21:59:44 +0200

    add train and evaluate documentation

--- evaluate.py
+++ evaluate.py
@@ -11,25 +11,35 @@ import pickle
 import sys
 
 def nn_train(author1, author2, model, w2v_path):
+    # evaluate neural network classifier
+    # define batch size
     batch_size = 128
+    # select device (CPU | GPU)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # create and build evaluation dataset
     dataset = TextDataset([author1, author2], norm=None, vectorizer='embed', w2v_path=w2v_path)
     X, _, Y, _ = dataset.build_dataset(eval=True)
+    # load model from file
     model = torch.load(model)
     model.to(device)
     model.eval()
     valid_steps = int(len(X) / batch_size)
     predictions = list()
+    # loop over all evaluation dataset
     for step in tqdm(range(valid_steps)):
+        # get x and y batches
         x_batch = X[step*batch_size:(step+1)*batch_size]
         x_batch = np.stack(x_batch, axis=0)
         x_batch = torch.from_numpy(x_batch).float().to(device)
         y_batch = Y[step*batch_size:(step+1)*batch_size]
         y_batch = np.stack(y_batch, axis=0)
+        # model forward pass
         y_out = model(x_batch)
         y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
+        # save predictions
         y_out = y_out > 0.5
         predictions.append(y_out)
+    # perform inference on the remaining samples
     x_batch = X[valid_steps*batch_size:]
     x_batch = np.stack(x_batch, axis=0)
     x_batch = torch.from_numpy(x_batch).float().to(device)
@@ -39,27 +49,36 @@ def nn_train(author1, author2, model, w2v_path):
     y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
     y_out = y_out > 0.5
     predictions.append(y_out)
+    # concatenate all predictions
     predictions = np.concatenate(predictions, axis=0)
+    # print results
     print (f'accuracy: {accuracy_score(Y, predictions)*100}%')
     print (f'logloss: {log_loss(Y, predictions)}')
 
 def lc_train(author1, author2, model):
+    # evaluate linear classifier model
+    # create and build evaluation dataset
     dataset = TextDataset([author1, author2], norm=None, vectorizer='tfidf')
     X, _, Y, _ = dataset.build_dataset(eval=True)
+    # load model from pickle
     model = pickle.load(open(model, 'rb'))
+    # predict dataset labels
     predictions = model.predict(X)
+    # print results
     print (f'accuracy: {accuracy_score(Y, predictions)*100}%')
     print (f'logloss: {log_loss(Y, predictions)}')
 
 if __name__ == "__main__":
+    # argument parsing
     argparser = argparse.ArgumentParser(description=__doc__)
     subparsers = argparser.add_subparsers(help='Sub-commands', dest='command')
+    # neural network subparser
     nn_subparser = subparsers.add_parser('eval-nn', help='Train neural network')
     nn_subparser.add_argument('--author1', help='path to first author writings', dest='author1', default='data/HPL_test.txt')
     nn_subparser.add_argument('--author2', help='path to second author writings', dest='author2', default='data/MWS_test.txt')
     nn_subparser.add_argument('--model', help='path to model file', dest='model', default='models/deep_model.pt')
     nn_subparser.add_argument('--w2v_path', help='path to word2vec file', dest='w2v_path', default='w2v_models/glove.840B.300d.txt')
-
+    # linear classifier subparser
     lc_subparser = subparsers.add_parser('eval-lc', help='Train linear classifier')
     lc_subparser.add_argument('--author1', help='path to first author writings', dest='author1', default='data/HPL_test.txt')
     lc_subparser.add_argument('--author2', help='path to second author writings', dest='author2', default='data/MWS_test.txt')
@@ -72,6 +91,7 @@ if __name__ == "__main__":
     author2 = kwargs.pop('author2')
     model = kwargs.pop('model')
 
+    # call evaluation function
     if subcmd is None:
         print ('Error: missing subcommand.  Re-run with --help for usage.')
         sys.exit(1)
--- train.py
+++ train.py
@@ -14,35 +14,48 @@ import json
 import sys
 
 def nn_train(config):
+    # train neural network classifier
+    # select device (CPU | GPU)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # create and build dataset
     dataset = TextDataset(config['txt_list'], norm=config['norm'], vectorizer=config['vectorizer'], w2v_path=config['w2v_path'])
     xtrain, xvalid, ytrain, yvalid = dataset.build_dataset()
+    # define network configuration
     network_config = {
         'emb_dim'         : config['emb_dim'],
         'rnn_hid_dim'     : config['rnn_hid_dim'],
         'dense_hid_dim' : config['dense_hid_dim'],
     }
+    # define model
     model = StylometryNN(network_config)
     model.to(device)
     model.train()
+    # define BCE loss
     criterion = nn.BCELoss()
+    # define optimizer
     optimizer = Adam(model.parameters(), lr=config['initial_lr'], weight_decay=config['weight_decay'])
     train_steps = int(len(xtrain) / config['batch_size'])
     valid_steps = int(len(xvalid) / config['batch_size'])
     best_accuracy = 0.5
+    # training loop
     for epoch in range(config['num_epochs']):
         total_loss = 0.0
+        # loop over all training dataset samples
         for step in tqdm(range(train_steps)):
+            # get x and y batches
             x_batch = xtrain[step*config['batch_size']:(step+1)*config['batch_size']]
             x_batch = np.stack(x_batch, axis=0)
             x_batch = torch.from_numpy(x_batch).float().to(device)
             y_batch = ytrain[step*config['batch_size']:(step+1)*config['batch_size']]
             y_batch = np.stack(y_batch, axis=0)
             y_batch = torch.from_numpy(y_batch).float().to(device)
+            # model forward pass
             y_out = model(x_batch)
             y_out = torch.squeeze(y_out, dim=1)
+            # calculate loss
             loss = criterion(y_out, y_batch)
             total_loss += loss.item()
+            # back propagation
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -50,39 +63,54 @@ def nn_train(config):
         model.eval()
         accuracies = list()
         losses = list()
+        # loop over all validation dataset samples
         for step in tqdm(range(valid_steps)):
+            # get x and y batches
             x_batch = xvalid[step*config['batch_size']:(step+1)*config['batch_size']]
             x_batch = np.stack(x_batch, axis=0)
             x_batch = torch.from_numpy(x_batch).float().to(device)
             y_batch = yvalid[step*config['batch_size']:(step+1)*config['batch_size']]
             y_batch = np.stack(y_batch, axis=0)
+            # model forward pass
             y_out = model(x_batch)
             y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
+            # calculate loss and accuracy
             y_out = y_out > 0.5
             accuracies.append(accuracy_score(y_batch, y_out))
             losses.append(log_loss(y_batch, y_out))
+        # print results
         print (f'Validation accuracy: {(sum(accuracies)/len(accuracies))*100}%')
         print (f'Validation logloss: {sum(losses)/len(losses)}')
+        # save model (based on best validation accuracy)
         if sum(accuracies)/len(accuracies) > best_accuracy:
             torch.save(model, 'models/deep_model.pt')
         model.train()
 
 def lc_train(config):
+    # train linear classifier (Naive Bayes)
+    # create and build dataset
     dataset = TextDataset(config['txt_list'], norm=config['norm'], vectorizer=config['vectorizer'])
     xtrain, xvalid, ytrain, yvalid = dataset.build_dataset()
+    # define model
     model = StylometryLC(truncation=config['truncation'])
+    # fit model
     model.fit(xtrain, ytrain)
+    # infer on validation data
     predictions = model.predict(xvalid)
+    # dump model pickle
     pickle.dump(model, open('models/nb_model.sav', 'wb'))
+    # print results
     print (f'accuracy: {accuracy_score(yvalid, predictions)*100}%')
     print (f'logloss: {log_loss(yvalid, predictions)}')
 
 if __name__ == "__main__":
+    # argument parsing
     argparser = argparse.ArgumentParser(description=__doc__)
     subparsers = argparser.add_subparsers(help='Sub-commands', dest='command')
+    # neural network subparser
     nn_subparser = subparsers.add_parser('train-nn', help='Train neural network')
     nn_subparser.add_argument('--config', help='path to network config file', dest='config', default='configs/nn_config.json')
-
+    # linear classifier subparser
     lc_subparser = subparsers.add_parser('train-lc', help='Train linear classifier')
     lc_subparser.add_argument('--config', help='path to model config file', dest='config', default='configs/lc_config.json')
 
@@ -91,9 +119,11 @@ if __name__ == "__main__":
     subcmd = kwargs.pop('command')
     config_file = kwargs.pop('config')
 
+    # read config JSON
     with open(config_file, 'r') as f:
         config = json.load(f)
 
+    # call training function
     if subcmd is None:
         print ('Error: missing subcommand.  Re-run with --help for usage.')
         sys.exit(1)
