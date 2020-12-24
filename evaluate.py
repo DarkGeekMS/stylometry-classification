fix logloss calculation

evaluate.py
@@ -2,6 +2,7 @@ from text_dataset import TextDataset
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from sklearn.metrics import log_loss, accuracy_score
 
 import numpy as np
@@ -37,7 +38,6 @@ def nn_train(author1, author2, model, w2v_path):
         y_out = model(x_batch)
         y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
         # save predictions
-        y_out = y_out > 0.5
         predictions.append(y_out)
     # perform inference on the remaining samples
     x_batch = X[valid_steps*batch_size:]
@@ -47,13 +47,13 @@ def nn_train(author1, author2, model, w2v_path):
     y_batch = np.stack(y_batch, axis=0)
     y_out = model(x_batch)
     y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
-    y_out = y_out > 0.5
     predictions.append(y_out)
     # concatenate all predictions
-    predictions = np.concatenate(predictions, axis=0)
+    predictions_proba = np.concatenate(predictions, axis=0)
+    predictions = predictions_proba > 0.5
     # print results
     print (f'accuracy: {accuracy_score(Y, predictions)*100}%')
-    print (f'logloss: {log_loss(Y, predictions)}')
+    print (f'logloss: {F.binary_cross_entropy(torch.from_numpy(predictions_proba).float(), torch.from_numpy(np.array(Y)).float())}')
 
 def lc_train(author1, author2, model):
     # evaluate linear classifier model
@@ -64,9 +64,10 @@ def lc_train(author1, author2, model):
     model = pickle.load(open(model, 'rb'))
     # predict dataset labels
     predictions = model.predict(X)
+    predictions_proba = model.predict_proba(X)
     # print results
     print (f'accuracy: {accuracy_score(Y, predictions)*100}%')
-    print (f'logloss: {log_loss(Y, predictions)}')
+    print (f'logloss: {log_loss(Y, predictions_proba)}')
 
 if __name__ == "__main__":
     # argument parsing

model.py
@@ -35,12 +35,21 @@ class StylometryLC:
             self.clf.fit(x, y)
 
     def predict(self, x):
-        # predict class (probability) of x given naive bayes
+        # predict class (label) of x given naive bayes
         if self.truncated:
             x_truncated = self.svd.transform(x)
             outputs = self.clf.predict(x_truncated)
         else:
             outputs = self.clf.predict(x)
+        return outputs
+
+    def predict_proba(self, x):
+        # predict class (probability) of x given naive bayes
+        if self.truncated:
+            x_truncated = self.svd.transform(x)
+            outputs = self.clf.predict_proba(x_truncated)
+        else:
+            outputs = self.clf.predict_proba(x)
         return outputs
 
 class StylometryNN(torch.nn.Module):

train.py
@@ -3,6 +3,7 @@ from model import StylometryLC, StylometryNN
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import Adam
 from sklearn.metrics import log_loss, accuracy_score
 
@@ -75,9 +76,9 @@ def nn_train(config):
             y_out = model(x_batch)
             y_out = torch.squeeze(y_out, dim=1).cpu().detach().numpy()
             # calculate loss and accuracy
-            y_out = y_out > 0.5
-            accuracies.append(accuracy_score(y_batch, y_out))
-            losses.append(log_loss(y_batch, y_out))
+            y_out_labels = y_out > 0.5
+            accuracies.append(accuracy_score(y_batch, y_out_labels))
+            losses.append(F.binary_cross_entropy(torch.from_numpy(y_out).float(), torch.from_numpy(y_batch).float()))
         # print results
         print (f'Validation accuracy: {(sum(accuracies)/len(accuracies))*100}%')
         print (f'Validation logloss: {sum(losses)/len(losses)}')
@@ -97,11 +98,12 @@ def lc_train(config):
     model.fit(xtrain, ytrain)
     # infer on validation data
     predictions = model.predict(xvalid)
+    predictions_proba = model.predict_proba(xvalid)
     # dump model pickle
     pickle.dump(model, open('models/nb_model.sav', 'wb'))
     # print results
     print (f'accuracy: {accuracy_score(yvalid, predictions)*100}%')
-    print (f'logloss: {log_loss(yvalid, predictions)}')
+    print (f'logloss: {log_loss(yvalid, predictions_proba)}')
 
 if __name__ == "__main__":
     # argument parsing